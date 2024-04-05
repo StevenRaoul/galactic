3642f5cb861731b0978d61623d071314cf88c6d6
StevenRaoul <sraoul@et.intechinfo.fr>
2024-04-05 14:24:29 +0200

    initialisation du projet

---
+++ src/main/java/com/jacoste/galactic/GalacticairlinesApplication.java
@@ -0,0 +1,13 @@
+package com.jacoste.galactic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalacticairlinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalacticairlinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jacoste/galactic/GalacticairlinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacoste.galactic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalacticairlinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
